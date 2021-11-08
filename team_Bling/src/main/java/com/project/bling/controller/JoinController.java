cbc0dc903183b12ddb8e81071da0a65a00dea241
MYCOM <MYCOM@DESKTOP-NAMEDPC>
2021-11-08 12:10:15 +0900

    회원가입

--- team_Bling/src/main/java/com/project/bling/controller/JoinController.java
+++ team_Bling/src/main/java/com/project/bling/controller/JoinController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.project.bling.service.JoinService;
+import com.project.bling.vo.UserVO;
 
 @RequestMapping(value="/Join")
 @Controller
@@ -16,8 +19,48 @@ public class JoinController {
 	@Autowired
 	JoinService joinService;
 	
+	//회원가입메인(아직 미정)
 	@RequestMapping(value="/main.do")
 	public String main(Locale locale, Model model) {
-		return "join/main";
+		return "join/addr";
+	}
+	
+	//일반회원가입
+	@RequestMapping(value="/normal.do")
+	public String normal(Locale locale,Model mode) {
+		return "join/normal";
+	}
+	
+	//회원가입시 정보들 db에 저장
+	@RequestMapping(value="/value.do", method = RequestMethod.POST)
+	public String insert(Locale locale,Model model,UserVO vo)throws Exception{
+		joinService.insert(vo);
+		return "login/main";
+	}
+	
+	//아이디 중복확인
+	@RequestMapping(value="/confirmid.do", method = RequestMethod.POST)
+	@ResponseBody
+	public String confirmid(Locale locale,Model model,String iddata)throws Exception{
+		String data = joinService.confirmid(iddata);
+		if(data==null) {
+			data="yes";
+		}else {
+			data="no";
+		}
+		return data;
+	}
+	
+	//이메일 중복확인
+	@RequestMapping(value="/confirmemail.do")
+	@ResponseBody
+	public String confirmemail(Locale locale,Model model,String emaildata)throws Exception{
+		String data = joinService.confirmemail(emaildata);
+		if(data==null) {
+			data="yes";
+		}else {
+			data="no";
+		}
+		return data;
 	}
 }
--- team_Bling/src/main/java/com/project/bling/service/JoinService.java
+++ team_Bling/src/main/java/com/project/bling/service/JoinService.java
@@ -1,5 +1,12 @@
 package com.project.bling.service;
 
-public interface JoinService {
+import com.project.bling.vo.UserVO;
 
+public interface JoinService {
+		//회원가입 db에 저장
+		void insert(UserVO vo) throws Exception;
+		//아이디 중복확인
+		String confirmid(String id) throws Exception;
+		//이메일 중복확인
+		String confirmemail(String email) throws Exception;
 }
