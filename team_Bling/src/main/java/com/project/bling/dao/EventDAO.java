22a257a67b4769e224db9de3ac99dc4e8719c275
woolfiekim <93753195+woolfiekim@users.noreply.github.com>
2021-12-01 09:24:45 +0900

    김주희 1201 오후 9시24분 홈 이벤트 부분

--- team_Bling/src/main/java/com/project/bling/dao/EventDAO.java
+++ team_Bling/src/main/java/com/project/bling/dao/EventDAO.java
@@ -1,8 +1,12 @@
 package com.project.bling.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.project.bling.vo.EventVO;
 
 @Repository
 public class EventDAO {
@@ -11,5 +15,9 @@ public class EventDAO {
 	private SqlSession sqlSession;
 	
 	private String em = "com.project.bling.mapper.eventMapper.";
+	
+	public List<EventVO> eventlist() throws Exception{
+		return sqlSession.selectList(em+"eventlist");
+	}
 	
 }
