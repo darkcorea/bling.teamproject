ed5338e5389e5495bd5202d7be9b69a391866789
woolfiekim <93753195+woolfiekim@users.noreply.github.com>
2021-12-06 16:56:48 +0900

    김주희 1206 오후4시 56분 개인정보수정 완료

--- team_Bling/src/main/java/com/project/bling/service/MyPageService.java
+++ team_Bling/src/main/java/com/project/bling/service/MyPageService.java
@@ -3,6 +3,7 @@ package com.project.bling.service;
 import java.util.List;
 
 import com.project.bling.vo.CombineVO;
+import com.project.bling.vo.UserVO;
 
 public interface MyPageService {
 
@@ -14,4 +15,9 @@ public interface MyPageService {
 	public void reviewWrite2(CombineVO vo) throws Exception;
 	public CombineVO reviewDetail(int ridx) throws Exception;
 	public void reviewDelete(int ridx) throws Exception;
+	
+	//회원정보수정 첫번째 비밀번호 확인시 갖고오는 이름정보
+	public UserVO confirm(int midx)throws Exception;
+	//회원정보 수정
+	public void remodify(UserVO uv)throws Exception;
 }
