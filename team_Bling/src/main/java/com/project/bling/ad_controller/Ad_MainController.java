4c0797707265bf268d5a9919eb5d108c92840978
woolfiekim <93753195+woolfiekim@users.noreply.github.com>
2021-12-08 14:55:14 +0900

    김주희 1208 오후 2시 55분 어드민 메인

--- team_Bling/src/main/java/com/project/bling/ad_controller/Ad_MainController.java
+++ team_Bling/src/main/java/com/project/bling/ad_controller/Ad_MainController.java
@@ -2,17 +2,24 @@ package com.project.bling.ad_controller;
 
 import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.project.bling.ad_service.Ad_MainService;
 
 @RequestMapping(value="/Ad_Main")
 @Controller
 public class Ad_MainController {
 
+	@Autowired
+	Ad_MainService ad_mainService;
+	
 	// 메인페이지 이동
 	@RequestMapping(value="/main.do")
-	public String main(Locale locale, Model model) {
+	public String main(Locale locale, Model model) throws Exception {
+		model.addAttribute("data",ad_mainService.order());
 		return "admin/main";
 	}
 }
--- team_Bling/src/main/java/com/project/bling/ad_service/Ad_MainServiceImpl.java
+++ team_Bling/src/main/java/com/project/bling/ad_service/Ad_MainServiceImpl.java
@@ -1,12 +1,20 @@
 package com.project.bling.ad_service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.project.bling.ad_dao.Ad_MainDAO;
+import com.project.bling.vo.DeliveryVO;
 
 @Service("Ad_MainService")
 public class Ad_MainServiceImpl implements Ad_MainService{
 	@Autowired
 	Ad_MainDAO ad_mainDAO;
+
+	@Override
+	public List<DeliveryVO> order() throws Exception {
+		return ad_mainDAO.order();
+	}
 }
