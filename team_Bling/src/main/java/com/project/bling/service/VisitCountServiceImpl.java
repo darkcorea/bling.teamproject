f5ddd46c57a44fafece2be82b89d1f42709fccd5
woolfiekim <93753195+woolfiekim@users.noreply.github.com>
2021-12-14 12:00:53 +0900

    김주희 1214 오후12시 방문자수 count

--- team_Bling/src/main/java/com/project/bling/service/VisitCountServiceImpl.java
+++ team_Bling/src/main/java/com/project/bling/service/VisitCountServiceImpl.java
@@ -1,18 +1,24 @@
 package com.project.bling.service;
 
+import javax.servlet.http.HttpSessionEvent;
+
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.project.bling.dao.VisitCountDAO;
 
-@Service("VisitCountService")
+@Service
 public class VisitCountServiceImpl implements VisitCountService{
-	@Autowired
-	VisitCountDAO visitcountDAO;
+	
+	//@Autowired
+	//VisitCountDAO visitcountDAO;
 
 	@Override
-	public void visiter() throws Exception {
-		visitcountDAO.visiter();
+	public void visiter(HttpSessionEvent hse,SqlSession sqlSession) throws Exception {
+		VisitCountDAO visitcountDAO = new VisitCountDAO();
+		System.out.println("9999999999999999999test"+visitcountDAO);
+		visitcountDAO.visiter(hse,sqlSession);
 	}
 
 }
